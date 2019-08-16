4c052635be23328c3b29cc45441cd82ee2d7b892
zw <herozhengwei@qq.com>
2019-08-16 18:48:59 +0800

    zhengwei12

--- src/beike0810_zhengwei/forth.java
+++ src/beike0810_zhengwei/forth.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class forth{
+    //aaa
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         String s = sc.nextLine();
