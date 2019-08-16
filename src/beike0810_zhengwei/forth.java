5bcb967eba8bf762b4cb7cc003db6da849da71bd
zw <herozhengwei@qq.com>
2019-08-16 17:52:53 +0800

    zhengwei

---
+++ src/beike0810_zhengwei/forth.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class forth{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] num = new int[n];
+        for(int i=0;i<n;i++){
+            num[i]=sc.nextInt();
+        }
+    }
+}
