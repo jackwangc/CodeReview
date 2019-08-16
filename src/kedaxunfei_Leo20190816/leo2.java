1d504dd919b02fdcfb4eb2ff723692c2595adc05
liyao0312 <370582412@qq.com>
2019-08-16 18:37:40 +0800

    ---

--- src/kedaxunfei_Leo20190816/leo2.java
+++ src/kedaxunfei_Leo20190816/leo2.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by artsing on 2019/8/16.
  */
 public class leo2 {
-    public static void main(String[] args) {
 
-    }
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            int[] arr = readArr(s);
+            //execute(arr, target);
+        }
+
+        public static int[] readArr(String s){
+            String s2 = s.substring(1,s.length()-1);
+            String[] strs = s2.split(",");
+            int[] arr = new int[strs.length];
+            for(int i =0; i<strs.length; i++){
+                arr[i] = Integer.parseInt(strs[i]);
+            }
+            return arr;
+        }
+
 }
