70dbc0055ce7774f0e60bc6b647ee4e0c4c3590e
zw <herozhengwei@qq.com>
2019-08-16 18:58:51 +0800

    zhengwei123

--- src/beike0810_zhengwei/forth.java
+++ src/beike0810_zhengwei/forth.java
@@ -7,16 +7,19 @@ public class forth{
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         String s = sc.nextLine();
-        if(s.length()==0){
-            System.out.print("-1");
-            return;
-        }
+        System.out.println(s);
         char[] chars = s.toCharArray();
         ArrayList<Integer> res = new ArrayList<>();
+        boolean bool = true;
         for (int i=0;i<chars.length;i++){
             if(chars[i]>='0' && chars[i]<='9'){
                 res.add(chars[i]-'0');
+                bool = false;
             }
+        }
+        if (bool){
+            System.out.print("-1");
+            return;
         }
         Collections.sort(res);
         for(int i=0;i<res.size();i++){
