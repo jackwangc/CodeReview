54e77a5e6f15551e078950deed22c84faa153791
liyao0312 <370582412@qq.com>
2019-08-16 19:17:33 +0800

    360

--- src/kedaxunfei_Leo20190816/leo2.java
+++ src/kedaxunfei_Leo20190816/leo2.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-/**
- * Created by artsing on 2019/8/16.
- */
 public class leo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int key =sc.nextInt();
+        String s = sc.nextLine();
+        //int key =sc.nextInt();
+        int[] arr = readArr(s);
+        int result =getKey(arr, 19,0,arr.length-1);
+        int b =(result==-1)?-1:result+1;
+        System.out.println(b);
+    }
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String s = scanner.nextLine();
-            int[] arr = readArr(s);
-            System.out.println(getKey(arr, 19,0,arr.length)+1);
+    public static int[] readArr(String s){
+        String s2 = s.substring(1,s.length()-1);
+        String[] strs = s2.split(",");
+        int[] arr = new int[strs.length];
+        for(int i =0; i<strs.length; i++){
+            arr[i] = Integer.parseInt(strs[i]);
         }
-
-        public static int[] readArr(String s){
-            String s2 = s.substring(1,s.length()-1);
-            String[] strs = s2.split(",");
-            int[] arr = new int[strs.length];
-            for(int i =0; i<strs.length; i++){
-                arr[i] = Integer.parseInt(strs[i]);
-            }
-            return arr;
-        }
+        return arr;
+    }
 
     public static int getKey(int[] arr, int key, int low, int high) {
         int mid = (low + high) / 2;
