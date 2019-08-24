e28dc743099059e06ff405adcded8bd584d2d98e
hlq <hlq@DESKTOP-VO24LMC>
2019-08-24 19:27:42 +0800

    wangyb test

--- src/jingdong/main.java
+++ src/jingdong/main.java
@@ -1,10 +1,18 @@
 package jingdong;
 
+import java.util.stream.Stream;
+
 /**
  * Created by hlq on 2019/8/24.
  */
+
 public class main {
     public static void main(String[] args){
-        System.out.println("hello world");
+        Stream.of("jd","jd.com","www.jd.com","www.jd.id")
+        .mapToInt(String::length)
+        .filter(len->len>3)
+        .peek(System.out::println)
+        .limit(2);
+        }
     }
-}
+
