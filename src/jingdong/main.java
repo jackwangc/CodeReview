590c3a2160d93a211e43863968e53048dd0e0ede
hlq <hlq@DESKTOP-VO24LMC>
2019-08-24 18:52:32 +0800

    wangyb test

---
+++ src/jingdong/main.java
@@ -0,0 +1,10 @@
+package jingdong;
+
+/**
+ * Created by hlq on 2019/8/24.
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
