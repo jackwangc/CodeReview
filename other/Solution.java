5c6c2f5a23dfc24b2b2972c9405447c90bfe59ed
郑伟 <cx_server01@163.com>
2019-08-28 19:32:56 +0800

    123

--- other/Solution.java
+++
@@ -1,8 +0,0 @@
-/**
- * lx,8.28
- */
-class Solution {
-    public void solution() {
-        
-    }
-}
--- src/dong/Second.java
+++
@@ -1,7 +0,0 @@
-package dong;
-
-/**
- * Created by WangJun on 2019/8/14.
- */
-public class Second {
-}
