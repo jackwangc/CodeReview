ecf1daa4a30049ad482ef5e6049dd060443977d0
liyao0312 <370582412@qq.com>
2019-09-05 20:24:21 +0800

    liyao  文件 留三个

--- src/leo/leo20.java
+++ src/leo/leo20.java
@@ -1,17 +1,42 @@
-import java.util.Scanner;
+import java.util.*;
 
-/**
- * Created by artsing on 2019/8/29.
- */
 public class leo20 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int count=0;
-        while(a!=0){
-            count++;
-            a=(a-1)&a;
+        Scanner sc =new Scanner(System.in);
+        String[] scs = sc.nextLine().split(" ");
+        int[] nums = new int[scs.length];
+        for(int i=0;i<scs.length;i++){
+            nums[i]=Integer.parseInt(scs[i]);
         }
-        System.out.println(count);
+        bubbleSort(nums);
+        for(int i=0;i<scs.length-1;i++){
+
+            System.out.print(nums[i]+",");
+
+        }
+        System.out.print(nums[scs.length-1]);
     }
+
+    private static void bubbleSort(int[] nums) {
+        if(nums.length==1||nums.length==0) return;
+
+        for (int i = nums.length - 1; i > 0; i--) {
+            boolean swapped = false;
+            for (int j = 0; j < i; j++) {
+                if(nums[j] > nums[j+1]){
+                    swapped = true;
+                    swap(nums,j,j+1);
+                }
+            }
+            if(!swapped)
+                return;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
 }
