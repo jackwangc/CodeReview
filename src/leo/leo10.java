bc1d749ad8acdb01623328a3d3f706b36fd14889
liyao0312 <370582412@qq.com>
2019-09-07 20:33:10 +0800

    New changelist

--- src/leo/leo10.java
+++ src/leo/leo10.java
@@ -1,38 +1,36 @@
-//import java.util.*;
-//
-//public class leo10 {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        String[] s=sc.nextLine().split(" ");
-//        int[] arr = new int[s.length];
-//        for(int i=0;i<s.length;i++){
-//            arr[i]= Integer.parseInt(s[i]);
-//        }
-//        int result= solution(arr);
-//        System.out.println(result);
-//
-//    }
-////    private static int solution(int[] arr) {
-////        int index=0;
-////        int steps=1;
-////        int minStep=-1;
-////        int tempStep=0;
-////        ArrayList<Integer> rs = new ArrayList<Integer>();
-////        for(int i=1;i<arr.length/2;i++){
-////            index+=i;
-////            for(int j=index;j<arr.length;j++){
-////                index+=arr[j];
-////                j=index;
-////                steps++;
-////                if(j==arr.length-1) {
-////                    tempStep=j;
-////                    break;
-////                }
-////            }
-////        }
-////
-////
-////    }
+import java.util.*;
+
+public class leo10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s=sc.nextLine().split(" ");
+        int[] arr = new int[s.length];
+        for(int i=0;i<s.length;i++){
+            arr[i]= Integer.parseInt(s[i]);
+        }
+        int result= solution(arr);
+        System.out.println(result);
+
+    }
+    private static int solution(int[] arr) {
+        int minStep=-1;
+        for(int i=1;i<arr.length/2;i++){
+            int index=i;
+            int steps=1;
+            for(int j=index;j<arr.length;){
+                j+=arr[j];
+                steps++;
+                if(j==arr.length-1) {
+                    if(minStep==-1||(minStep!=-1&&minStep>steps)){
+                        minStep=steps;
+                    }
+                }
+                if(j>arr.length-1) break;
+            }
+        }
+       return minStep;
+
+    }
 //    private static int solution1(int[] arr) {
 //        int end = 0;
 //        int maxPosition = 0;
@@ -47,6 +45,6 @@
 //        return steps;
 //
 //    }
-//
-//
-//}
+
+
+}
