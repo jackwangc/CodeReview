de63f96a8eec1a8b513101a94215cf9b92415ced
liyao0312 <370582412@qq.com>
2019-09-07 19:49:22 +0800

    New changelist

--- src/leo/leo50.java
+++ src/leo/leo50.java
@@ -1,37 +1,33 @@
 import java.util.*;
 
-public class leo50 {
+public class leo20 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
-        int m =sc.nextInt();
-        int k =sc.nextInt();
-        int[][] obstacle = new int[k][2];
-        for(int i=0;i<k;i++){
-            obstacle[i][0] = sc.nextInt();
-            obstacle[i][1] = sc.nextInt();
-        }
-        //int result = solution(n,m,obstacle);
-        int result = 2;
-        System.out.println(result);
+        int m = sc.nextInt();
+        int re = combine(n,m).size();
 
+        System.out.print(re%1000000007);
     }
-
-
-    static int solution(int[] prices, int budget) {
-        if(prices==null||prices.length==0) return -1;
-        Arrays.sort(prices);
-        int count=0;
-        for(int i=prices.length-1;i>=0;i--){
-            while (budget>=prices[i]){
-                int num=budget/prices[i];
-                count+=num;
-                budget=budget%prices[i];
-            }
-        }
-        if(count==0) return -1;
-        else return count;
-
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        getAns(1,n, k, new ArrayList<Integer>(), ans);
+        return ans;
     }
-
+    private static void getAns(int start, int n, int k, ArrayList<Integer> temp,List<List<Integer>> ans) {
+        //如果 temp 里的数字够了 k 个，就把它加入到结果中
+        if(temp.size() == k){
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        //从 start 到 n
+        for (int i = start; i <= n; i++) {
+            //将当前数字加入 temp
+            temp.add(i);
+            //进入递归
+            getAns(i+1, n, k, temp, ans);
+            //将当前数字删除，进入下次 for 循环
+            temp.remove(temp.size() - 1);
+        }
+     }
 }
