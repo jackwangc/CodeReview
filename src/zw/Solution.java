2c2844cea2ac6fdaa3e6e1150e3c85dd024c4647
郑伟 <cx_server01@163.com>
2019-09-17 20:23:18 +0800

    1

--- src/zw/Solution.java
+++ src/zw/Solution.java
@@ -1,38 +1,32 @@
 package zw;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String ss[] = s.split(",");
-        int num = Integer.valueOf(ss[1]);
-        System.out.print(ss[0],num);
-        System.out.print("11111");
-    }
-    public static String convert(String s, int numRows) {
-
-        if (numRows == 1) return s;
-
-        List<StringBuilder> rows = new ArrayList<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++)
-            rows.add(new StringBuilder());
-
-        int curRow = 0;
-        boolean goingDown = false;
-
-        for (char c : s.toCharArray()) {
-            rows.get(curRow).append(c);
-            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
-            curRow += goingDown ? 1 : -1;
+        String ss[] = s.split(" ");
+        int n = Integer.valueOf(ss[0]);
+        int m = Integer.valueOf(ss[1]);
+        int k = Integer.valueOf(ss[2]);
+        if (k==m*n){
+            System.out.print(0);
+            return;
         }
-
-        StringBuilder ret = new StringBuilder();
-        for (StringBuilder row : rows) ret.append(row);
-        return ret.toString();
+        int res = Integer.MAX_VALUE;
+        int left;
+        int i;
+        for (i=0;i<n;i++){
+            left = n-i;
+            int right_max = k/left;
+            if (right_max>=m){
+                res = Math.min(res,i);
+            }else {
+                res = Math.min(res,i+(m-right_max));
+            }
+        }
+        System.out.println(Math.min(m,Math.min(n,res)));
     }
+
 }
