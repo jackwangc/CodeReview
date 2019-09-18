e17084e801c1d4273e7441d8d26d46ef40867860
liyao0312 <370582412@qq.com>
2019-09-18 21:45:36 +0800

    New changelist

--- src/leo/leo20.java
+++ src/leo/leo20.java
@@ -1,34 +1,35 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class leo20 {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        int k = Integer.parseInt(sc.nextLine());
-        String s =sc.nextLine();
-        System.out.println(solution(s,k));
+        String[] s = sc.nextLine().split(";");
+        String re =solution(s[0],s[1])?"True":"False";
+        System.out.println(re);
     }
 
 
-    public static int solution(String s, int k) {
+    public static boolean solution(String s1,String s2) {
+        int length = s1.length();
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+        Map m = new HashMap();
 
-        int re = 0;
-        int l = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int right = 0; right < s.length(); right++) {
-            map.put(s.charAt(right),right);
-            while (map.size() > k) {
-                char leftChar = s.charAt(l);
-                if (map.get(leftChar) == l) {
-                    map.remove(leftChar);
-                }
-                l++;
+        for(int i=0; i<length; i++)
+        {
+            if(m.get(ch1[i]) != null){
+                ch1[i] =(char) m.get(ch1[i]);
+            }else{
+                if(m.containsValue(ch2[i]))return  false;
+                m.put(ch1[i],ch2[i]);
+                ch1[i] = ch2[i];
             }
-            int subResult = right - l + 1;
-            re = Math.max(re,subResult);
+            if(ch1[i] != ch2[i])    return false;
         }
-        return re;
 
+        return true;
     }
 }
--- src/leo/leo30.java
+++ src/leo/leo30.java
@@ -1,24 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class leo30 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        String[] scs = sc.nextLine().split("],");
-
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] strs = str.substring(1,str.length()-1).split(", ");
+        int[] nums = new int[strs.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+        System.out.println(FindGreatestSumOfSubArray(nums));
     }
 
-
-
-    public int maxArea(int[] height) {
-        int m = 0, l = 0, r = height.length - 1;
-        while (l < r) {
-            m = Math.max(m, Math.min(height[l], height[r]) * (r - l));
-            if (height[l] > height[r])
-                r--;
-            else
-                l++;
+    public static int FindGreatestSumOfSubArray(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int greatestSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int val : nums) {
+            sum = sum <= 0 ? val : sum + val;
+            greatestSum = Math.max(greatestSum, sum);
         }
-        return m;
+        return greatestSum;
     }
 }
