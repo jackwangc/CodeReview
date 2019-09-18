8f13a79f50ec95c8392af4e8333466f6a44b10a1
郑伟 <cx_server01@163.com>
2019-09-18 17:54:27 +0800

    2

---
+++ src/zw/Solution34.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String sn = sc.nextLine();
+        int n = Integer.valueOf(sn);
+        String[] str = new String[n];
+        for (int i=0;i<n;i++){
+            String s = sc.nextLine();
+            str[i] = s;
+        }
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return String.valueOf(o1.charAt(0)).compareTo(String.valueOf(o2.charAt(0)));
+            }
+        });
+        for (int i=0;i<n;i++){
+            System.out.println(str[i]);
+        }
+    }
+}
